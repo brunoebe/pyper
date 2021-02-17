4bb10163ed12465e43e5c7967a955cd103ed93ec
Bruno Ebé <brunoebe@gmail.com>
2021-02-17 21:01:09 +0100

    Changing license to GNU Lesser General Public License v3.0

--- widgets/spreadsheet/__init__.py
+++ widgets/spreadsheet/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright: (C) 2014 Bruno Ébé
 # Author: Bruno Ébé | contact@brunoebe.com
-# License: GNU General Public License v3.0 | https://www.gnu.org/licenses
+# License: GNU Lesser General Public License v3.0 | https://www.gnu.org/licenses
 
 """
 A widget example that display a list of parameters as spreadsheets.
@@ -10,16 +10,16 @@ Copyright: (C) 2014 Bruno Ébé
 Author: Bruno Ébé | contact@brunoebe.com
 License:
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
     
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
     
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
